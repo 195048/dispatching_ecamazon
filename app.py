b6a818459ab8dcbaa8962c510d934c85f46019bd
RayaneS <78797911+195048@users.noreply.github.com>
2023-12-02 12:40:11 +0100

    First api commit

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient('mongodb://localhost:27017/')
+db = client['ECAMamazon']
+colis_collection = db['colis']
+livreurs_collection = db['livreurs']
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/colis', methods=['POST'])
+def recevoir_colis():
+    data = request.json
+    colis_collection.insert_many(data['colis'])
+    return jsonify({'message': 'Colis reçus avec succès'})
+
+@app.route('/livreurs/<int:id_livreur>/colis', methods=['GET'])
+def get_liste_colis(id_livreur):
+    liste_colis_livreur = colis_collection.find({'livreur_id': id_livreur, 'etat': 0})
+    return jsonify({'colis': [colis for colis in liste_colis_livreur]})
+
+@app.route('/livreurs/<int:id_livreur>/geolocalisation', methods=['POST'])
+def update_geolocalisation(id_livreur):
+    data = request.json
+    livreurs_collection.update_one({'_id': id_livreur}, {'$set': {'x': data['x'], 'y': data['y']}})
+    return jsonify({'message': 'Géolocalisation mise à jour avec succès'})
+
+@app.route('/colis/<int:id_colis>/livraison', methods=['POST'])
+def update_etat_livraison(id_colis):
+    data = request.json
+    colis_collection.update_one({'_id': id_colis}, {'$set': {'etat': data['etat']}})
+
+    # Effectuer un POST vers une autre API
+    autre_api_url = "https://url_de_l_autre_api"
+    payload = {'id_colis': id_colis, 'etat': data['etat']}
+    
+    try:
+        response = requests.post(autre_api_url, json=payload)
+        response.raise_for_status()
+        return jsonify({'message': 'État de livraison mis à jour avec succès et POST effectué avec succès vers l\'autre API'})
+    except requests.exceptions.RequestException as e:
+        return jsonify({'message': f'État de livraison mis à jour avec succès, mais erreur lors du POST vers l\'autre API: {str(e)}'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,54 @@
 import unittest
-from app import app
+import json
+from app import app, colis_collection, livreurs_collection
 
 class TestApp(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
+        self.livreur_id = 1
+        self.colis_id = 1
 
-    def test_hello(self):
-        response = self.app.get('/')
-        self.assertEqual(response.data, b'Hello, World!')
+    def test_recevoir_colis(self):
+        data = {'colis': [{'_id': self.colis_id, 'Xadresse': 1, 'Yadresse': 1}]}
+        response = self.app.post('/colis', json=data)
+        self.assertEqual(response.status_code, 200)
+
+        # Vérifie que le colis est bien dans la base de données
+        colis = colis_collection.find_one({'_id': self.colis_id})
+        self.assertIsNotNone(colis)
+
+    def test_get_liste_colis(self):
+        # Ajoute un colis pour le livreur
+        colis_collection.insert_one({'_id': self.colis_id, 'Xadresse': 1, 'Yadresse': 1, 'livreur_id': self.livreur_id, 'etat': 0})
+
+        response = self.app.get(f'/livreurs/{self.livreur_id}/colis')
+        data = json.loads(response.get_data(as_text=True))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('colis', data)
+        self.assertEqual(len(data['colis']), 1)
+
+    def test_update_geolocalisation(self):
+        data = {'x': 2, 'y': 2}
+        response = self.app.post(f'/livreurs/{self.livreur_id}/geolocalisation', json=data)
+        self.assertEqual(response.status_code, 200)
+
+        # Vérifie que la géolocalisation du livreur est mise à jour dans la base de données
+        livreur = livreurs_collection.find_one({'_id': self.livreur_id})
+        self.assertIsNotNone(livreur)
+        self.assertEqual(livreur['x'], data['x'])
+        self.assertEqual(livreur['y'], data['y'])
+
+    def test_update_etat_livraison(self):
+        data = {'etat': 1}
+        response = self.app.post(f'/colis/{self.colis_id}/livraison', json=data)
+        self.assertEqual(response.status_code, 200)
+
+        # Vérifie que l'état de livraison du colis est mis à jour dans la base de données
+        colis = colis_collection.find_one({'_id': self.colis_id})
+        self.assertIsNotNone(colis)
+        self.assertEqual(colis['etat'], data['etat'])
 
 if __name__ == '__main__':
     unittest.main()
