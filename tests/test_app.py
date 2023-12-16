f2faf788fb31f84c5532ae8273a91db16fab58da
RayaneS <78797911+195048@users.noreply.github.com>
2023-12-16 15:57:43 +0100

    added confirmation of delivery

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,104 @@
 import unittest
-from app import app, db, Colis
+from unittest.mock import patch, MagicMock
+from flask import Flask
+from your_app_module import app, is_mysql_available, add_colis, post_route2, post_route3, get_route
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-        app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        # Set up a test client
         self.app = app.test_client()
-        db.create_all()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+    @patch("your_app_module.mysql.connector.connect")
+    def test_is_mysql_available(self, mock_connect):
+        # Mock the MySQL connection
+        mock_connect.return_value = MagicMock()
 
-    def test_recevoir_colis(self):
-        data = {'colis': [{'IDcolis': '123', 'Xadresse': 1.0, 'Yadresse': 2.0}]}
-        response = self.app.post('/colis', json=data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json, {'message': 'Colis reçus avec succès'})
+        # Call the function and assert
+        result = is_mysql_available()
+        self.assertTrue(result)
 
-        # Vérifier que le colis a été ajouté à la base de données
-        colis = Colis.query.filter_by(IDcolis='123').first()
-        self.assertIsNotNone(colis)
-        self.assertEqual(colis.Xadresse, 1.0)
-        self.assertEqual(colis.Yadresse, 2.0)
+    @patch("your_app_module.mysql.connector.connect")
+    def test_add_colis(self, mock_connect):
+        # Mock the MySQL connection
+        mock_connect.return_value = MagicMock()
 
-    # Ajoutez d'autres tests selon vos besoins
+        # Mock the request data
+        mock_request_data = {
+            "id": 1,
+            "adresse_x": 123,
+            "adresse_y": 456
+        }
+
+        # Mock the MySQL cursor
+        mock_cursor = mock_connect.return_value.cursor.return_value
+        mock_cursor.fetchone.return_value = (1, )  # Mock the result of the SELECT query
+
+        # Make the request to the endpoint
+        response = self.app.post('/add_colis', json=mock_request_data)
+
+        # Assert the response status code
+        self.assertEqual(response.status_code, 201)
+
+        # Add more assertions if needed
+
+    @patch("your_app_module.requests.post")
+    def test_post_route2(self, mock_post):
+        # Mock the POST request to the other microservice
+        mock_post.return_value.status_code = 200
+
+        # Mock the request data
+        mock_request_data = {
+            "colis_id": 1,
+            "etat_colis": 1
+        }
+
+        # Make the request to the endpoint
+        response = self.app.post('/postPosColisFromDevice', json=mock_request_data)
+
+        # Assert the response status code
+        self.assertEqual(response.status_code, 201)
+
+        # Add more assertions if needed
+
+    @patch("your_app_module.requests.post")
+    def test_post_route3(self, mock_post):
+        # Mock the POST request to the other microservice
+        mock_post.return_value.status_code = 200
+
+        # Mock the request data
+        mock_request_data = {
+            "camion_id": 1,
+            "camion_pos_x": 123,
+            "camion_pos_y": 456
+        }
+
+        # Make the request to the endpoint
+        response = self.app.post('/postPosCamionFromDevice', json=mock_request_data)
+
+        # Assert the response status code
+        self.assertEqual(response.status_code, 201)
+
+        # Add more assertions if needed
+
+    @patch("your_app_module.mysql.connector.connect")
+    @patch("your_app_module.mysql.connector.connect")
+    def test_get_route(self, mock_connect1, mock_connect2):
+        # Mock the MySQL connections
+        mock_connect1.return_value = MagicMock()
+        mock_connect2.return_value = MagicMock()
+
+        # Mock the MySQL cursor
+        mock_cursor = mock_connect2.return_value.cursor.return_value
+        mock_cursor.fetchall.return_value = [(1, 2, 3, 4, 0)]  # Mock the result of the SELECT query
+
+        # Make the request to the endpoint
+        response = self.app.get('/getLivraison')
+
+        # Assert the response status code
+        self.assertEqual(response.status_code, 201)
+
+        # Add more assertions if needed
+
 
 if __name__ == '__main__':
     unittest.main()
